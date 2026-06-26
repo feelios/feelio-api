7d76a5c6f545564b5d9399d5665591c2795dbae2
BeautifulSoup35 <als477049@gmail.com>
2026-06-26 16:27:06 +0900

    springboot설치

---
+++ src/main/java/com/korit/feelioapi/FeelioApiApplication.java
@@ -0,0 +1,13 @@
+package com.korit.feelioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelioApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeelioApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/korit/feelioapi/FeelioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korit.feelioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeelioApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
